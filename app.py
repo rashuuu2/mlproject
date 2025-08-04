f2ff09c616da7b797a313462a67b4c3639e9bbff
rashuuu2 <rashisingh67899@gmail.com>
2025-08-04 22:20:47 +0530

    Deployment

--- app.py
+++ app.py
@@ -9,8 +9,7 @@ application=Flask(__name__)
 
 app=application
 
-## Route for a home page
-
+#Route for a home page
 @app.route('/')
 def index():
     return render_template('index.html') 
@@ -20,27 +19,31 @@ def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
     else:
-        data=CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+        try:
+            data=CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('reading_score')),
+                writing_score=float(request.form.get('writing_score'))
+            )
+            pred_df=data.get_data_as_data_frame()
+            print('DataFrame for prediction:', pred_df)
+            predict_pipeline=PredictPipeline()
+            results=predict_pipeline.predict(pred_df)
+            print('Prediction results:', results)
+            return render_template('home.html',results=results[0])
+        except Exception as e:
+            import traceback
+            print('Exception occurred during prediction:')
+            traceback.print_exc()
+            return render_template('home.html', results=None, error=str(e))
 
-        )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-
-        predict_pipeline=PredictPipeline()
-        results=predict_pipeline.predict(pred_df)
-        return render_template('home.html',results=results[0])
-    
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
+    app.run(host="0.0.0.0", port=5000)
 
 
 
